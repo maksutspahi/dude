7bfae1cbcbfd433406951436608f866d3b7637af
Maksut Spahi <maksut.spahi@prime.com.tr>
2016-11-17 10:16:06 +0300

    first controller

---
+++ src/main/java/com/dude/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.dude.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by maksutspahi on 17/11/16.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/index")
+    public String greeting(Model model) {
+        model.addAttribute("name", "ANIL");
+        return "index";
+    }
+}
